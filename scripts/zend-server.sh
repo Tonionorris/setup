e9f45ccc60bf62b4d6cf74625d1516858213fea4
Frédéric Dewinne <frederic.dewinne@vaconsulting.lu>
2012-11-27 22:56:05 +0100

    check profile before update

--- scripts/zend-server.sh
+++ scripts/zend-server.sh
@@ -8,7 +8,10 @@ deb http://repos.zend.com/zend-server/6.0/beta/deb server non-free" > /etc/apt/s
 apt-get update
 apt-get install -y zend-server-php-5.4
 
-echo "PATH=$PATH:/usr/local/zend/bin
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/zend/lib" >> /etc/profile
-source /etc/profile
+grep_output="$(grep \"\/usr\/local\/zend\/lib\" /etc/profile)"
 
+if [ -z "$grep_output" ]; then
+	echo "PATH=$PATH:/usr/local/zend/bin
+	LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/zend/lib" >> /etc/profile
+	source /etc/profile
+fi
