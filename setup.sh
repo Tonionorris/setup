58147a0f58428e27d7e9d218b5537aee5e14bf28
Frédéric Dewinne <frederic.dewinne@vaconsulting.lu>
2013-02-06 14:01:12 +0100

    add global bash setup script
    Change-Id: I6f50b320901ac7927c2b44575939a4387bd2240d

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+apt-get install dialog -y
+
+mariadb=""
+zend-server=""
+subversion=""
+git=""
+jenkins=""
+webmin=""
+
+# open fd
+exec 3>&1
+ 
+# Store data to $VALUES variable
+VALUES=$(dialog --ok-label "Install" \
+	  --backtitle "" \
+	  --title "Setup your server" \
+	  --checklist "Choose packages to install" \
+15 70 0 \
+	"mariadb" "MariaDB server" off \
+	"zend-server" "Zend Server" off \
+	"subversion" "SVN client" off \
+	"git" "git client" off \
+	"jenkins" "Jenkins continuous integration server" off \
+	"webmin" "Webmin management interface" off \
+2>&1 1>&3)
+ 
+# close fd
+exec 3>&-
+clear
+
+# update installed
+apt-get update
+apt-get dist-upgrade -y
+
+# prepare temp folder
+mkdir -p /tmp/setup
+cd /tmp/setup
+rm -Rf *
+
+# get proxy setup file
+wget https://raw.github.com/vaconsulting/setup/master/scripts/_setup.sh
+chmod +x _setup.sh
+
+# display values just entered
+for PACKAGE in $VALUES
+do
+	./_setup.sh $PACKAGE
+done
