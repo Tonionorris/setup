3828c144e9e37f3da3d16dd204a3336342f05bef
Frédéric Dewinne <frederic.dewinne@vaconsulting.lu>
2013-02-13 21:58:11 +0100

    fix xdebug install bug
    Change-Id: I7808c207518c4f3e73f7f99a794957411b4bad40

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -6,41 +6,34 @@ PECL=/usr/local/zend/bin/pecl
 PEAR=/usr/local/zend/bin/pear
 
 ZEND_DEBUGGER_CONF=/usr/local/zend/etc/conf.d/debugger.ini
-ZEND_EXT_MANAG_CONF=/usr/local/zend/etc/conf.d/extension_manager.ini
-XDEBUG_EXT=zend_extension=/usr/local/zend/lib/php_extensions/xdebug.so
+PHP_CONF=/usr/local/zend/etc/php.ini
+XDEBUG_EXT=; Xdebug \
+zend_extension = /usr/local/zend/lib/php_extensions/xdebug.so \
+xdebug.remote_enable=1 \
+xdebug.remote_handler=dbgp \
+xdebug.remote_mode=req \
+xdebug.remote_host=127.0.0.1 \
+xdebug.remote_port=9000
 
 # install xdebug for zend server
+/usr/local/zend/bin/zendctl.sh stop
 apt-get update
-apt-get install build-essential fakeroot checkinstall -y
-cd /usr/src
-wget http://ftp.gnu.org/gnu/m4/m4-1.4.9.tar.gz
-tar -zvxf m4-1.4.9.tar.gz
-rm m4-1.4.9.tar.gz
-cd m4-1.4.9/
-./configure && make && make install
-cd /usr/src
-wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.62.tar.gz
-tar -zvxf autoconf-2.62.tar.gz
-rm autoconf-2.62.tar.gz
-cd autoconf-2.62/
-./configure && make && make install
+apt-get install build-essential automake -y
 $PECL install xdebug
 
 # disable zend debugger
 DISABLED=$(cat $ZEND_DEBUGGER_CONF | grep -c ';zend_extension_manager\.dir\.debugger')
 if [ $DISABLED -eq 0 ] ; then
-	sed '2,2s/zend_extension_manager\.dir\.debugger/;zend_extension_manager\.dir\.debugger/' \
+	sed '%s/zend_extension_manager\.dir\.debugger/;zend_extension_manager\.dir\.debugger/' \
 	$ZEND_DEBUGGER_CONF > $ZEND_DEBUGGER_CONF.tmp
 	rm $ZEND_DEBUGGER_CONF
 	mv $ZEND_DEBUGGER_CONF.tmp $ZEND_DEBUGGER_CONF
 fi
-SETUP=$(cat $ZEND_EXT_MANAG_CONF | grep -c $XDEBUG_EXT)
+SETUP=$(cat $PHP_CONF | grep -c "xdebug")
 if [ $SETUP -eq 0 ] ; then
-	sed "2i$XDEBUG_EXT" $ZEND_EXT_MANAG_CONF > $ZEND_EXT_MANAG_CONF.tmp
-	rm $ZEND_EXT_MANAG_CONF
-	mv $ZEND_EXT_MANAG_CONF.tmp $ZEND_EXT_MANAG_CONF
+	echo $XDEBUG_EXT > $PHP_CONF
 fi
-/usr/local/zend/bin/zendctl.sh restart
+/usr/local/zend/bin/zendctl.sh start
 
 # install phpunit
 $PEAR install --alldeps phpunit/PHPUnit
