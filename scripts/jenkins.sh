f9ad5af84537eb22369c12e64e637dba77538fb8
Frédéric Dewinne <frederic.dewinne@vaconsulting.lu>
2013-02-17 14:04:16 +0100

    fix xdebug install bug

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -2,41 +2,7 @@
 
 
 JENKINS="java -jar /usr/local/bin/jenkins-cli.jar -s http://localhost:8080"
-PECL=/usr/local/zend/bin/pecl
 PEAR=/usr/local/zend/bin/pear
-
-ZEND_DEBUGGER_CONF=/usr/local/zend/etc/conf.d/debugger.ini
-PHP_CONF=/usr/local/zend/etc/php.ini
-XDEBUG_EXT=; Xdebug \
-zend_extension = /usr/local/zend/lib/php_extensions/xdebug.so \
-xdebug.remote_enable=1 \
-xdebug.remote_handler=dbgp \
-xdebug.remote_mode=req \
-xdebug.remote_host=127.0.0.1 \
-xdebug.remote_port=9000
-
-# install xdebug for zend server
-/usr/local/zend/bin/zendctl.sh stop
-apt-get update
-apt-get install build-essential automake -y
-$PECL install xdebug
-
-# disable zend debugger
-DISABLED=$(cat $ZEND_DEBUGGER_CONF | grep -c ';zend_extension_manager\.dir\.debugger')
-if [ $DISABLED -eq 0 ] ; then
-	sed '%s/zend_extension_manager\.dir\.debugger/;zend_extension_manager\.dir\.debugger/' \
-	$ZEND_DEBUGGER_CONF > $ZEND_DEBUGGER_CONF.tmp
-	rm $ZEND_DEBUGGER_CONF
-	mv $ZEND_DEBUGGER_CONF.tmp $ZEND_DEBUGGER_CONF
-fi
-SETUP=$(cat $PHP_CONF | grep -c "xdebug")
-if [ $SETUP -eq 0 ] ; then
-	echo $XDEBUG_EXT > $PHP_CONF
-fi
-/usr/local/zend/bin/zendctl.sh start
-
-# install phpunit
-$PEAR install --alldeps phpunit/PHPUnit
 
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 
--- scripts/zend-server.sh
+++ scripts/zend-server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PEAR=/usr/local/zend/bin/pear
+PECL=/usr/local/zend/bin/pecl
 
 VERSION="5.4"
 wget http://repos.zend.com/zend.key -O- |apt-key add -
@@ -9,7 +10,7 @@ echo "# Zend Server
 deb http://repos.zend.com/zend-server/6.0/deb_ssl1.0 server non-free" > /etc/apt/sources.list.d/zend-server.list
 
 apt-get update
-apt-get install -y zend-server-php-$VERSION php-$VERSION-extra-extensions-zend-server
+apt-get install -y zend-server-php-$VERSION build-essential automake
 
 grep_output=$(grep -c "\/usr\/local\/zend\/lib" /etc/profile)
 
@@ -18,6 +19,8 @@ if [ $grep_output -eq 0 ]; then
 LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/zend/lib" >> /etc/profile
 	source /etc/profile
 fi
+
+$PECL install xdebug
 
 $PEAR update-channels
 $PEAR channel-discover pear.phpunit.de
