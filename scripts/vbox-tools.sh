5b1450751fcc0132a7d63d5d54748ca534bbb9a3
Antoine Delamarre <antoine.delamarre@vaconsulting.lu>
2013-02-18 12:04:45 +0100

    debug virtual box guest addition script
    Change-Id: If6dc30707c82d47762e1222f167172de251cdf10
    Signed-off-by: Antoine Delamarre <antoine.delamarre@vaconsulting.lu>

--- scripts/vbox-tools.sh
+++ scripts/vbox-tools.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+clear
+mount /dev/cdrom /media/cdrom
+
+while [ ! -f /media/cdrom/VBoxLinuxAdditions.run ]
+do
+	read response -p "Please, insert the Virtual box guest additions disc and press [o]k or [a]bort"
+	if [ $response eq "a" ]
+	then
+		exit
+	fi
+	clear
+	mount /dev/cdrom /media/cdrom
+done
+
 dist=$(uname -r)
 apt-get install dkms build-essential $dist
 apt-get install gcc
-mount /dev/cdrom /media/cdrom
 sudo ./media/cdrom/VBoxLinuxAdditions.run
