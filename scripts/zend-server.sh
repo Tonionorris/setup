fdfcdf40b8a04f5e2d3625684d0d1c145179b8c6
Frédéric Dewinne <frederic.dewinne@vaconsulting.lu>
2013-02-26 11:37:22 +0100

    fix syntax error

--- scripts/zend-server.sh
+++ scripts/zend-server.sh
@@ -24,7 +24,7 @@ $PECL install xdebug
 xdebug_enabled=$(grep -c "xdebug" /usr/local/zend/etc/php.ini)
 
 if [ $xdebug_enabled -eq 0 ]; then
-    echo "\
+    echo "
 zend_extension=\"/usr/local/zend/lib/php_extensions/xdebug.so\"" >> /usr/local/zend/etc/php.ini
 fi
 
